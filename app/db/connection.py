6a6be64a1d4e008be0f9b036efb2dc1f61ae04d2
codewithdevi <jpaquinol44@gmail.com>
2026-05-24 19:34:43 +0800

    chore: add database connection setup

---
+++ app/db/connection.py
@@ -0,0 +1,32 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Handle the possible error for the database connection
+if DATABASE_URL:
+   if DATABASE_URL.startswith("postgres://"):
+      DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg://", 1)
+   elif DATABASE_URL.startswith("postgresql://"):
+      DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
+
+# This is a guard just in case
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set. Check your .env file.")
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+   pass
+
+def get_db():
+   db = SessionLocal()
+   try:
+      yield db
+   finally:
+      db.close()
